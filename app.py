2d80f2193c7e42a4389b131d499dcf0d404c478b
jeremie640 <sourbe.jeremie@orange.fr>
2022-03-04 17:39:06 +0100

    add streamlit app.py file completed

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-import numpy as np
-import pandas as pd
+components.html("""
+    hello world
+    <div id="container" style="width: 450px; height: 600px">
+      coco
+    </div>
 
-st.markdown("""# This is a header
-## This is a sub header
-This is text""")
+    <script type="module">
+    import Aaron from 'https://cdn.skypack.dev/@freesewing/aaron';
+    import theme from 'https://cdn.skypack.dev/@freesewing/plugin-theme';
+    import svgAttr from 'https://cdn.skypack.dev/@freesewing/plugin-svgattr';
 
-df = pd.DataFrame({
-          'first column': list(range(1, 11)),
-          'second column': np.arange(10, 101, 10)
-        })
+    const svg = new Aaron({ 
+        sa: 10, // Seam allowance
+        paperless: true, // Enable paperless mode
+        // More settings, see: https://FreeSewing.dev/reference/api/settings
+        measurements: { // Pass in measurements
+        biceps: 387,
+        chest: 1105,
+        hips: 928,
+        hpsToWaistBack: 502,
+        neck: 420,
+        shoulderSlope: 13,
+        shoulderToShoulder: 481,
+        waistToHips: 139,
+        }
+    })
+    .use(theme)
+    .use(svgAttr, {width: "100%", height: "100%"})
+    .draft() // Draft the pattern
+    .render()
 
-# this slider allows the user to select a number of lines
-# to display in the dataframe
-# the selected value is returned by st.slider
-line_count = st.slider('Select a line count', 1, 10, 3)
-
-# and used in order to select the displayed lines
-head_df = df.head(line_count)
-
-head_df
+    document.getElementById('container').innerHTML = svg
+  </script>""", height=600)
